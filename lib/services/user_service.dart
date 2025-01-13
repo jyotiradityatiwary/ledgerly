b45fc1ba33a54051af7fddd9e0d8afcbc2cbf578
Jyotiraditya Tiwary <78427496+jyotiradityatiwary@users.noreply.github.com>
2025-01-14 00:30:48 +0530

    Add update and modify functions for user service

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,23 +1,15 @@
-// CREATE TABLE Users (
-//   user_id INTEGER PRIMARY KEY,
-//   user_name TEXT NOT NULL,
-//   currency_precision INT NOT NULL,
-//   currency TEXT NOT NULL,
-//   cloud_user_id INTEGER DEFAULT NULL UNIQUE REFERENCES CloudUsers(cloud_user_id)
-// )
-
 import 'package:ledgerly/services/cloud_user_service.dart';
 import 'package:ledgerly/services/database_service.dart';
 import 'package:sqlite3/sqlite3.dart';
 
 class User {
-  int id;
-  String name;
-  int currencyPrecision;
-  String currency;
-  CloudUser? cloudUser;
+  final int id;
+  final String name;
+  final int currencyPrecision;
+  final String currency;
+  final CloudUser? cloudUser;
 
-  User({
+  const User({
     required this.id,
     required this.name,
     required this.currencyPrecision,
@@ -34,7 +26,7 @@ class User {
       );
 
   static const createTableSql = '''
-CREATE TABLE Users (
+CREATE TABLE $sqlTable (
   user_id INTEGER PRIMARY KEY,
   user_name TEXT NOT NULL,
   currency_precision INT NOT NULL,
@@ -44,6 +36,12 @@ CREATE TABLE Users (
   static const sqlCols =
       'user_id, user_name, currency_precision, currency, cloud_user_id';
   static const sqlColsQuestions = '?, ?, ?, ?, ?';
+  static const sqlUpdateClauseWithoutId =
+      'user_name = ?, currency_precision = ?, currency = ?, cloud_user_id = ?';
+  static const sqlTable = 'Users';
+
+  List<Object?> toListWithoutId() =>
+      [name, currencyPrecision, currency, cloudUser?.id];
 
   @override
   int get hashCode => id;
@@ -52,19 +50,19 @@ CREATE TABLE Users (
 }
 
 List<int> getAllUserIds() {
-  const String sql = 'SELECT ROWID FROM Users;';
+  const String sql = 'SELECT ROWID FROM ${User.sqlTable};';
   final ResultSet results = db.select(sql);
   return [for (Row row in results) row.columnAt(0) as int];
 }
 
 List<User> getAllUsers() {
-  const String sql = 'SELECT ${User.sqlCols} FROM Users;';
+  const String sql = 'SELECT ${User.sqlCols} FROM ${User.sqlTable};';
   final ResultSet results = db.select(sql);
   return [for (final Row row in results) User.fromRow(row)];
 }
 
 User getUser(int id) {
-  const sql = 'SELECT ${User.sqlCols} FROM Users WHERE ROWID = ?;';
+  const sql = 'SELECT ${User.sqlCols} FROM ${User.sqlTable} WHERE ROWID = ?;';
   final ResultSet results = db.select(sql, [id]);
   final Row row = results[0];
   return User.fromRow(row);
@@ -76,7 +74,20 @@ int registerUser({
   required currency,
 }) {
   const sql =
-      'INSERT INTO Users(${User.sqlCols}) VALUES (${User.sqlColsQuestions});';
+      'INSERT INTO ${User.sqlTable}(${User.sqlCols}) VALUES (${User.sqlColsQuestions});';
   db.execute(sql, [null, name, currencyPrecision, currency, null]);
   return db.lastInsertRowId;
+}
+
+void deleteUser({required int id}) {
+  const sql = "DELETE FROM ${User.sqlTable} WHERE ROWID = ? RETURNING ROWID;";
+  final ResultSet results = db.select(sql, [id]);
+  assert(results.length == 1);
+}
+
+void updateUser(User user) {
+  const sql =
+      "UPDATE ${User.sqlTable} SET ${User.sqlUpdateClauseWithoutId} WHERE ROWID = ? RETURNING ROWID;";
+  final ResultSet results = db.select(sql, [user.toListWithoutId()]);
+  assert(results.length == 1);
 }
--- test/database_test.dart
+++ test/database_test.dart
@@ -43,6 +43,7 @@ void main() {
 
   group('User Service', () {
     setUpAll(() {
+      if (File(dbPath).existsSync()) deleteDatabase();
       initializeDatabase(path: dbPath);
     });
 
@@ -52,12 +53,10 @@ void main() {
     });
 
     test('Can we register and retrieve users to/from database?', () {
-      const numOfUsers = 4;
+      const int numOfUsers = 4;
+      assert(numOfUsers > 0);
       int? previousId;
-      for (final int i in Iterable.generate(
-        numOfUsers,
-        (index) => index + 1,
-      )) {
+      for (final int i in Iterable.generate(numOfUsers)) {
         final name = 'User $i';
         final precision = 4 + (i % 2);
         const currencies = ['\$', '₹', 'EUR'];
@@ -88,6 +87,57 @@ void main() {
           reason: 'Returned values should match inserted values',
         );
       }
+
+      final userIds = getAllUserIds();
+      expect(
+        userIds.length,
+        equals(numOfUsers),
+        reason:
+            'Number of (returned) user id-s should equal the number of users we inserted.',
+      );
+
+      final users = getAllUsers();
+      expect(
+        users.map((user) => user.id).toList(),
+        equals(userIds),
+        reason:
+            'ID of returned users should match the list of ids returned by the getAllUserIDs function.',
+      );
+
+      expect(
+        () {
+          deleteUser(id: previousId!);
+        },
+        returnsNormally,
+        reason: 'This user exists so deletion should occur normally.',
+      );
+      expect(getAllUserIds().length, numOfUsers - 1,
+          reason:
+              'Number of users in databse should decrease by one on deletion.');
+      expect(
+        () {
+          deleteUser(id: previousId!);
+        },
+        throwsAssertionError,
+        reason:
+            'This user is already deleted so function should fail assertion.',
+      );
+    });
+
+    test('Update User', () {
+      const String name = "User";
+      const int currencyPrecision = 4;
+      const String currency = "EUR";
+      final int id = registerUser(
+          name: name, currencyPrecision: currencyPrecision, currency: currency);
+      expect(
+          getUser(id).toListWithoutId(),
+          equals(User(
+            id: id,
+            name: name,
+            currencyPrecision: currencyPrecision,
+            currency: currency,
+          ).toListWithoutId()));
     });
   });
 }
