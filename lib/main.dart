8517d77644d9d93ac9b422c9369baabbcc19f8f2
Jyotiraditya Tiwary <78427496+jyotiradityatiwary@users.noreply.github.com>
2025-04-16 14:08:05 +0530

    Add dynamic colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ledgerly/views/app_initializer.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,12 +23,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ledgerly',
-      theme: ThemeData.light(useMaterial3: true),
-      darkTheme: ThemeData.dark(useMaterial3: true),
-      home: AppInitializer(),
-      themeMode: ThemeMode.system,
+    return DynamicColorBuilder(
+      builder: (
+        final ColorScheme? lightDynamic,
+        final ColorScheme? darkDynamic,
+      ) {
+        final lightTheme = lightDynamic == null
+            ? ThemeData.light()
+            : ThemeData(
+                colorScheme: lightDynamic,
+                brightness: Brightness.light,
+              );
+        final darkTheme = darkDynamic == null
+            ? ThemeData.dark()
+            : ThemeData(
+                colorScheme: darkDynamic,
+                brightness: Brightness.dark,
+              );
+        return MaterialApp(
+          title: 'Ledgerly',
+          theme: lightTheme,
+          darkTheme: darkTheme,
+          home: AppInitializer(),
+          themeMode: ThemeMode.system,
+        );
+      },
     );
   }
 }
